7c13b27062ba55c75e80871a09a2dde8885ed333
YAGGO ROSA DE ALMEIDA SILVA <rosayaggo2@gmail.com>
2025-09-18 16:13:50 -0300

    finalização da segunda atividade

--- Atividade-2/pages/produtos.php
+++ Atividade-2/pages/produtos.php
@@ -7,8 +7,23 @@
 //Qtd: 08
 //c-Atualize o preço de todos os produto e exiba todos eles novamente, simulando um desconto de 10% em todos os produtos.
 
-$moveis = ["nome " => " Tv "," preco " => " R$ 3000,00 "," estoque " => " 2000 "] ;
+$moveis = [
+    [ "nome" => "TV", "preco" => 5600.00, "quantidade" => 8], ["nome" => "Computador","preco" => 3500.00, "quantidade" => 5], [ "nome" => "Sofá","preco" => 2800.00,"quantidade" => 10],["nome" => "Geladeira","preco" => 4200.00,"quantidade" => 3], ["nome" => "Cabeceira","preco" => 800.00,"quantidade" => 12 ]];
+ 
 
-foreach ($moveis as $itens => $resposta) {
-    echo $itens . "=" . $resposta;
+echo "<h3>Lista de Produtos:</h3>";
+foreach ($moveis as $movel) {
+    echo "Produto: {$movel['nome']} <br>";
+    echo "Preço: R$ " . number_format($movel['preco'], 2, ",", ".") . "<br>";
+    echo "Qtd: {$movel['quantidade']} <br><br>";
 }
+ 
+
+echo "<h3>Lista de Produtos com 10% de desconto:</h3>";
+foreach ($moveis as $movel) {
+    $resultado = $movel['preco'] * 0.90; // desconto de 10%
+    echo "Produto: {$movel['nome']} <br>";
+    echo "Preço: R$ " . number_format($resultado, 2, ",", ".") . "<br>";
+    echo "Qtd: {$movel['quantidade']} <br><br>";
+}
+?>
