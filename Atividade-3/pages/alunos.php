192cd1d615f6198b232e4de412fc3ce9a095bf1f
YAGGO ROSA DE ALMEIDA SILVA <rosayaggo2@gmail.com>
2025-09-19 14:24:43 -0300

    finalização

--- Atividade-3/pages/alunos.php
+++ Atividade-3/pages/alunos.php
@@ -14,16 +14,16 @@ $alunos = [
     ["nome" => "Sophia", "idade" => 18, "notaFinal" => 10],
     ["nome" => "Felipe", "idade" => 18, "notaFinal" => 7],
 //os 10 alunos
-    ["nome" => "Ana", "idade" => 20, "notaFinal" => 6], 
-    ["nome" => "Carlos", "idade" => 23, "notaFinal" => 7], 
+    ["nome" => "Ana", "idade" => 18, "notaFinal" => 6], 
+    ["nome" => "Camila", "idade" => 19, "notaFinal" => 7], 
     ["nome" => "Fernanda", "idade" => 21, "notaFinal" => 8], 
-    ["nome" => "Ricardo", "idade" => 26, "notaFinal" => 9],
-    ["nome" => "Joana", "idade" => 22, "notaFinal" => 2],
-    ["nome" => "Marcos", "idade" => 24, "notaFinal" => 8],
-    ["nome" => "Patrícia", "idade" => 23, "notaFinal" => 6],
-    ["nome" => "Felipe", "idade" => 25, "notaFinal" => 7],
-    ["nome" => "Bianca", "idade" => 20, "notaFinal" => 9],
-    ["nome" => "André", "idade" => 21, "notaFinal" => 8]
+    ["nome" => "Eduardo", "idade" => 17, "notaFinal" => 9],
+    ["nome" => "Thayná", "idade" => 18, "notaFinal" => 2],
+    ["nome" => "Luci", "idade" => 19, "notaFinal" => 8],
+    ["nome" => "Yuri", "idade" => 20, "notaFinal" => 6],
+    ["nome" => "Miguel", "idade" => 17, "notaFinal" => 7],
+    ["nome" => "Bianca", "idade" => 16, "notaFinal" => 9],
+    ["nome" => "André", "idade" => 18, "notaFinal" => 8]
 ];
  
 echo "Lista de Alunos:" . "<br>";
